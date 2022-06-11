3756b13461df6fb486f87f14f50b7b72ab0ba7b7
Ilia <ilabrosimov@gmail.com>
2022-06-11 16:16:04 +0300

    Добавил UITableViewController

--- Project1_StormViewer/ViewController.swift
+++ Project1_StormViewer/ViewController.swift
@@ -7,10 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var pictures = [String]()
     override func viewDidLoad() {
-        var pictures = [String]()
+       
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let fm = FileManager.default
@@ -22,8 +22,16 @@ class ViewController: UIViewController {
             }
         }
         print(pictures)
+            
     }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
 
-
+    }
 }
 
